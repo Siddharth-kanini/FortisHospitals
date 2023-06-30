989c9ecd56b14186c1c63490747fe1165fb52da8
Siddharth-kanini <127303066+Siddharth-kanini@users.noreply.github.com>
2023-07-01 01:43:57 +0530

    Add project files.

---
+++ Doctor/Program.cs
@@ -0,0 +1,34 @@
+using Doctorapp.Repositories;
+using Doctorapp.Services;
+using Microsoft.EntityFrameworkCore;
+using ModelLibrary.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<HospitalContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("Fortisapp")));
+builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
+builder.Services.AddScoped<DoctorService>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
